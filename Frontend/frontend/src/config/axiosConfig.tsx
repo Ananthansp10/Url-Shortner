86a7fc4c679548ac75075adb83994d99864df8f1
Ananthan sp <spananthan920@gmail.com>
2025-10-31 15:00:12 +0530

    dotenv added in frontend

--- Frontend/frontend/src/config/axiosConfig.tsx
+++ Frontend/frontend/src/config/axiosConfig.tsx
@@ -1,7 +1,10 @@
 import axios from "axios";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const api = axios.create({
-  baseURL: process.env.NEXT_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: process.env.NEXT_API_BASE_URL || "http://localhost:5000/api" ,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
